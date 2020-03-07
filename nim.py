6357691036152f0fe1eb663386ee9b49cb327da5
burnpiro <kemalpiro@gmail.com>
2020-03-07 14:52:40 +0100

    Add types to Nim

--- nim.py
+++ nim.py
@@ -1,37 +1,51 @@
+from __future__ import annotations
 import random
+from typing import List, Tuple
 from functools import reduce
 
 MINIMUM_INITIAL_PILE_SIZE = 1
 
+Action = Tuple[int, int]
 
-class nim:
-    def __init__(self, piles=3, objects=20, seed=None):
+
+class Nim:
+    def __init__(self, piles: int = 3, objects: int = 20, seed: int or None = None) -> None:
         random.seed(seed)
-        assert objects >= piles*MINIMUM_INITIAL_PILE_SIZE
-        objects -= piles*MINIMUM_INITIAL_PILE_SIZE
-        self.piles = [MINIMUM_INITIAL_PILE_SIZE for _ in range(piles)]
-        self.current_player = 0
-        self.done = False
+        assert objects >= piles * MINIMUM_INITIAL_PILE_SIZE
+        objects -= piles * MINIMUM_INITIAL_PILE_SIZE
+        self.piles: List[int] = [MINIMUM_INITIAL_PILE_SIZE for _ in range(piles)]
+        self.current_player: int = 0
+        self.done: bool = False
+
+        # distribute objects randomly into piles
         for _ in range(objects):
-            self.piles[random.randint(0, piles-1)] += 1
+            self.piles[random.randint(0, piles - 1)] += 1
 
-    def possible_actions(self):
-        return reduce(lambda a, b: a+b, [[(i, j) for j in range(1, pile+1)] for i, pile in enumerate(self.piles)])
+    # Generate list of actions base on piles size and number
+    def possible_actions(self) -> List[Action]:
+        return reduce(lambda acc, list_of_actions: acc + list_of_actions,
+                      [[(pile_idx, j) for j in range(1, pile + 1)] for pile_idx, pile in enumerate(self.piles)])
 
-    def act(self, action):
+    def act(self, action: Action) -> bool:
         pile, objects_taken = action
+
+        # Make sure that pile size has enough objects to take from
         assert self.piles[pile] >= objects_taken
         self.piles[pile] -= objects_taken
-        self.current_player = 1-self.current_player
-        self.done = reduce(lambda a, b: a+b, self.piles) == 0
+        self.current_player = 1 - self.current_player
+
+        # Count number of objects left and check if game is done
+        self.done = reduce(lambda acc, pile_size: acc + pile_size, self.piles) == 0
         return self.done
 
-    def copy(self):
-        _copy = nim(0, 0, 0)
-        _copy.piles = self.piles.copy()
+    def copy(self) -> Nim:
+        _copy = Nim(0, 0, 0)
+        _copy.piles = self.piles[:]
         _copy.current_player = self.current_player
         _copy.done = self.done
         return _copy
-# game = nim(3, 24, 0)
+
+
+# game = Nim(3, 24, 0)
 # print(game.piles)
 # print(game.possible_actions())
