d465ba5068c15988148ea4920a8da75a036cf734
Marek Pokropiński <marek.pokropinski@outlook.com>
2020-03-14 21:40:30 +0100

    Add 2 layer mode

--- run_gym_random.py
+++ run_gym_random.py
@@ -1,21 +1,23 @@
 import gym
 from gym import wrappers, logger
 
+from pong_game import PongGame
+
+
 class RandomAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
 
     def act(self, observation, reward, done):
-        return self.action_space.sample()
+        return self.action_space.sample(), self.action_space.sample()
+
 
 if __name__ == '__main__':
     # You can set the level to logger.DEBUG or logger.WARN if you
     # want to change the amount of output.
     logger.set_level(logger.INFO)
 
-    env = gym.make('Pong-v0')
-    outdir = '/tmp/random-agent-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
+    env = PongGame()
     env.seed(0)
     agent = RandomAgent(env.action_space)
 
@@ -27,9 +29,9 @@ if __name__ == '__main__':
     for i in range(episode_count):
         ob = env.reset()
         while True:
-            action = agent.act(ob, reward, done)
+            action1, action2 = agent.act(ob, reward, done)
             # print(action)
-            ob, reward, done, _ = env.step(action)
+            ob, reward, done, _ = env.step(action1, a2=action2)
             # print(reward)
             # print(ob.size)
             if done:
